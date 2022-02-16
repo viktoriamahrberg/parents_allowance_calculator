fdf3412b5b2008f32e1bee5bee702127eec25eb2
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-16 07:43:35 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_income_data():
+    """
+    Collects the gross annual salary from user
+    """
+    print("Please enter your annual gross salary (5-7 digits only)\nExample: 450000")
+    data_str = input("Enter your salary here:")
+    print(data_str)
+get_income_data()
