ffdf7ec01277c8ac13a9692e5a5208af2fea50f1
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-16 08:32:04 +0000

    Add introduction text and input data from user function

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+print("\nWelcome to your Parent's Allowance Calculator. \nYou can use this calculator to find out about what your annual pay when on maternity-/paternity leave will be.")
+print("The allowance is based on 80% of your gross salary and a 30% tax deduction on top of that.\n")
+
 def get_income_data():
     """
     Collects the gross annual salary from user
     """
-    print("Please enter your annual gross salary (5-7 digits only)\nExample: 450000")
-    data_str = input("Enter your salary here:")
+    print("Please enter your annual gross salary (5-7 digits only)\nExample: 450000\n")
+    data_str = input("Enter your salary here:\n")
     print(data_str)
+
 get_income_data()
