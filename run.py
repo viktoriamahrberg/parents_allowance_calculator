c9ab7e0f94e5c2ede182b1ad3e8c8dc347f3299f
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-16 10:13:29 +0000

    Add validate_data function to throw error message if input data is not inserted correctly

--- run.py
+++ run.py
@@ -7,6 +7,20 @@ def get_income_data():
     """
     print("Please enter your annual gross salary (5-7 digits only)\nExample: 450000\n")
     data_str = input("Enter your salary here:\n")
-    print(data_str)
+    validate_data(data_str)
+
+def validate_data(values): 
+    """
+    Checks if data (salary) has been inserted as values and throws an error message if it's wrongly input.
+    """
+    
+    try: 
+        return int(values)
+        print(f"You entered {values}")
+
+    except ValueError:
+        print("You entered the salary incorrectly. Please enter a valid number")
+        
+
 
 get_income_data()
