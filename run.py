f2a59d4613235cd291221a5cd0d2f22f834867e2
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-17 06:45:11 +0000

    add main function

--- run.py
+++ run.py
@@ -5,9 +5,6 @@ class bcolors:
     WARNING = '\033[93m'
     ENDC = '\033[0m'
 
-print("\nWelcome to your Parent's Allowance Calculator. \nYou can use this calculator to find out about what your annual pay when on maternity-/paternity leave will be.")
-print("The allowance is based on 80% of your gross salary and a 30% tax deduction on top of that.\n")
-
 def get_income_data():
     """
     Collects the gross annual salary from user
@@ -35,15 +32,25 @@ def validate_annual_data(values):
         print(bcolors.WARNING + "You entered the salary incorrectly. Please enter a valid number\n" + bcolors.ENDC)
         return False
         
-    return True
+    return annual_salary
 
 def calculate_annual_allowance(values):
     """
     Calculates yearly allowance based on yearly salary; 80% of gross salary minus taxes of 70%
     """
-    print(values)
+    print("we get to here")
+    print(type(values))
 
 
-data_str = get_income_data()
-calculate_annual_allowance(data_str)
-#calculate_annual_allowance()
+
+def main():
+    print("\nWelcome to your Parent's Allowance Calculator. \nYou can use this calculator to find out about what your annual pay when on maternity-/paternity leave will be.")
+    print("The allowance is based on 80% of your gross salary and a 30% tax deduction on top of that.\n")
+    wage = get_income_data()
+    validated_wage = validate_annual_data(wage)
+    # you will need to write a return to the function below to keep this process going
+    # calc_amount = calculate_annual_allowance(validated_wage)
+    print(type(validated_wage))
+
+    
+main()
