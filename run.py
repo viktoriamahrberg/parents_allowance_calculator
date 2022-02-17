f426af323ecc401b90932797faa77a44be197b74
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-17 10:52:43 +0000

    Return total yearly allowance successfully

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
-import math
-
-
-# Class from StockOverflow
-
+"""
+Color error message feature from StockOverflow
+"""
 WARNING = '\033[93m'
 ENDC = '\033[0m'
+OKCYAN = '\033[96m'
 
 
 def get_income_data():
@@ -23,10 +22,11 @@ def get_income_data():
 
 def validate_annual_data(values):
     """
-    Checks if data (salary) has been inserted as values and throws an error message if it's wrongly input.
+    Checks if data (salary) has been inserted as values
+    and throws an error message if it's wrongly input.
     """
     try:
-        annual_salary = int(values) 
+        annual_salary = int(values)
         print(f"You entered {annual_salary}")
 
     except ValueError:
@@ -37,16 +37,22 @@ def validate_annual_data(values):
     return annual_salary
 
 
-def calculate_annual_allowance(values):
+def calculate_annual_allowance(wage):
     """
-    Calculates yearly allowance based on yearly salary; 
+    Calculates yearly allowance based on yearly salary;
     80% of gross salary minus taxes of 70%
     """
 
+    annual_allowance = (wage * 0.8 * 0.7)
+    print(OKCYAN+f"Your total annual allowance is: {annual_allowance}" + ENDC)
 
 
 def main():
-    print("\nWelcome to your Parent's Allowance Calculator. \nYou can use this calculator to find out about what your annual pay when on maternity-/paternity leave will be.")
+    """
+    Run the program functions
+    """
+    print("\nWelcome to your Parent's Allowance Calculator.\n")
+    print("You can use this calculator to find out about what your annual pay when on maternity-/paternity leave will be.")
     print("The allowance is based on 80% of your gross salary and a 30% tax deduction on top of that.\n")
     wage = get_income_data()
     calculate_annual_allowance(wage)
